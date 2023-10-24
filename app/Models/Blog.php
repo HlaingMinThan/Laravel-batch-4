4fb50d55051a62a8b67f11a7aa9180724743a3f5
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-10-24 22:56:35 +0630

    laravel day-1

---
+++ app/Models/Blog.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\File;
+
+class Blog
+{
+    public static function all()
+    {
+        $blogs = [];
+        $files = File::files(resource_path('blogs'));
+        foreach ($files as $file) {
+            $blogs[] = $file->getContents();
+        }
+        return $blogs;
+    }
+
+    public static function find($filename)
+    {
+        $blog = file_get_contents(resource_path('/blogs/' . $filename . '.html'));
+
+        return $blog;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Models\Blog;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('blogs', [
+        'blogs' => Blog::all()
+    ]);
+});
+Route::get('/blogs/{filename}', function ($filename) {
+    return view('blog-detail', [
+        'blog' => Blog::find($filename)
+    ]);
+});
